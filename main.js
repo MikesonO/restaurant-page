32652e192f9db8f3f2430005cbbd78a10064137a
mikeson <mikeson.jnr@gmail.com>
2022-10-02 21:12:23 +0100

    Import createHeader to index.js

--- main.js
+++ main.js
@@ -1,5 +1,231 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _images_cow_icon_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+
+
+function createNavigationBar() {
+
+  function createHeader() {
+    const header = document.createElement("header");
+
+    header.classList.add("header");
+    header.appendChild(createLinks());
+
+    return header;
+  }
+
+  const createLinks = () => {
+    const links = document.createElement("ul");
+
+    links.append(
+      createHomeLink(),
+      createMenuLink(),
+      createLogoLink(),
+      createAboutLink(),
+      createContactLink()
+    );
+
+    return links;
+  };
+
+   //Create Home Nav-Item
+  function createHomeLink() {
+    const home = document.createElement("li");
+    const homeLink = document.createElement("a");
+
+    homeLink.textContent = "Home";
+    homeLink.setAttribute("href","#");
+
+    home.appendChild(homeLink);
+    return home;
+  }
+
+  //Create Menu Nav-Item
+  function createMenuLink() {
+    const menu = document.createElement("li");
+    const menuLink = document.createElement("a");
+
+    menuLink.textContent = "Menu";
+    menuLink.setAttribute("href","#");
+
+    menu.appendChild(menuLink);
+    return menu;
+  }
+
+  //Create About Nav-Item
+  function createAboutLink() {
+    const about = document.createElement("li");
+    const aboutLink = document.createElement("a");
+
+    aboutLink.textContent = "About";
+    aboutLink.setAttribute("href","#");
+
+    about.appendChild(aboutLink);
+    return about;
+  }
+
+
+    //Create Logo Nav-Item
+    function createLogoLink() {
+      const logo = document.createElement("li");
+      const logoLink = document.createElement("a");
+      const logoText = document.createElement("h1");
+      const logoImage = document.createElement("img");
+
+      logoImage.setAttribute("src",_images_cow_icon_png__WEBPACK_IMPORTED_MODULE_0__["default"]);
+      logoImage.setAttribute("width", "40px")
+      logoImage.setAttribute("alt","Restaurant Logo")
+      logoText.textContent="Steak House";
+      logoLink.setAttribute("href","#");
+
+      logoText.appendChild(logoImage);
+      logoLink.appendChild(logoText);
+      logo.appendChild(logoLink);
+      return logo;
+    }
+  
+
+  //Create Contact Link
+  function createContactLink() {
+    const contact = document.createElement("li");
+
+    const contactLink = document.createElement("a");
+    contactLink.textContent = "Contact";
+    contactLink.setAttribute("href","#");
+
+    contact.appendChild(contactLink);
+    return contact;
+  }
+
+  return createHeader();
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createNavigationBar());
+
+/***/ }),
+/* 2 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "7ff930e8aa47d8bc90a384d5238bdf14.png");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
-console.log("Pizza!");
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_structure_header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+
+
+function pageLoad() {
+  const content = document.getElementById("content");
+  console.log(content);
+  content.appendChild(_modules_structure_header__WEBPACK_IMPORTED_MODULE_0__["default"]);
+
+}
+
+pageLoad();
+})();
+
 /******/ })()
 ;
