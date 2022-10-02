51ee6f4d28d43cb3d00e58d458282df732998c93
mikeson <mikeson.jnr@gmail.com>
2022-10-02 20:36:34 +0100

    Set up file-loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
   },
 };
